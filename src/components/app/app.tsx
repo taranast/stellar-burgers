9f66681fa0a0d8716e8bfd524ad83c7181628d19
taranast <taranast434@gmail.com>
2026-09-19 23:31:05 +0300

    feat: add routes to public pages and modals

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,15 +1,20 @@
-import { ConstructorPage } from '@pages';
+import { ConstructorPage, Feed, NotFound404 } from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
 
-import { AppHeader } from '@components';
+import { AppHeader, IngredientDetails, Modal, OrderInfo } from '@components';
 import { Preloader } from '@ui';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 
 const App = () => {
   /** TODO: взять переменные из стора */
   const isIngredientsLoading = false;
   const ingredients = [];
   const error = null;
+  const navigate = useNavigate();
+  const onClose = () => {
+    navigate(-1);
+  };
 
   return (
     <div className={styles.app}>
@@ -21,7 +26,27 @@ const App = () => {
           {error}
         </div>
       ) : ingredients.length > 0 ? (
-        <ConstructorPage />
+        <Routes>
+          <Route path='/' element={<ConstructorPage />} />
+          <Route path='/feed' element={<Feed />} />
+          <Route path='*' element={<NotFound404 />} />
+          <Route
+            path='/feed/:number'
+            element={
+              <Modal title='' onClose={onClose}>
+                <OrderInfo />
+              </Modal>
+            }
+          />
+          <Route
+            path='/ingredients/:id'
+            element={
+              <Modal title='' onClose={onClose}>
+                <IngredientDetails />
+              </Modal>
+            }
+          />
+        </Routes>
       ) : (
         <div className={`${styles.title} text text_type_main-medium pt-4`}>
           Нет игредиентов
